c32c4f375020dcfa43ffefa88652be11db98051f
Daniel Allen <dan@aeternagroup.net>
2026-02-11 23:53:10 +0000

    Update to schema version v4.59.0

--- variables.tf
+++ variables.tf
@@ -45,10 +45,10 @@ EOT
     name                       = string
     resource_group_name        = string
     disabled_data_sources      = optional(set(string))
-    enabled                    = optional(bool, true)
+    enabled                    = optional(bool) # Default: true
     events_to_export           = optional(set(string))
     log_analytics_workspace_id = optional(string)
-    log_unmasked_ips_enabled   = optional(bool, false)
+    log_unmasked_ips_enabled   = optional(bool) # Default: false
     query_for_resources        = optional(string)
     query_subscription_ids     = optional(set(string))
     tags                       = optional(map(string))
@@ -57,22 +57,22 @@ EOT
       workspace_id = string
     }))
     recommendations_enabled = optional(object({
-      acr_authentication               = optional(bool, true)
-      agent_send_unutilized_msg        = optional(bool, true)
-      baseline                         = optional(bool, true)
-      edge_hub_mem_optimize            = optional(bool, true)
-      edge_logging_option              = optional(bool, true)
-      inconsistent_module_settings     = optional(bool, true)
-      install_agent                    = optional(bool, true)
-      ip_filter_deny_all               = optional(bool, true)
-      ip_filter_permissive_rule        = optional(bool, true)
-      open_ports                       = optional(bool, true)
-      permissive_firewall_policy       = optional(bool, true)
-      permissive_input_firewall_rules  = optional(bool, true)
-      permissive_output_firewall_rules = optional(bool, true)
-      privileged_docker_options        = optional(bool, true)
-      shared_credentials               = optional(bool, true)
-      vulnerable_tls_cipher_suite      = optional(bool, true)
+      acr_authentication               = optional(bool) # Default: true
+      agent_send_unutilized_msg        = optional(bool) # Default: true
+      baseline                         = optional(bool) # Default: true
+      edge_hub_mem_optimize            = optional(bool) # Default: true
+      edge_logging_option              = optional(bool) # Default: true
+      inconsistent_module_settings     = optional(bool) # Default: true
+      install_agent                    = optional(bool) # Default: true
+      ip_filter_deny_all               = optional(bool) # Default: true
+      ip_filter_permissive_rule        = optional(bool) # Default: true
+      open_ports                       = optional(bool) # Default: true
+      permissive_firewall_policy       = optional(bool) # Default: true
+      permissive_input_firewall_rules  = optional(bool) # Default: true
+      permissive_output_firewall_rules = optional(bool) # Default: true
+      privileged_docker_options        = optional(bool) # Default: true
+      shared_credentials               = optional(bool) # Default: true
+      vulnerable_tls_cipher_suite      = optional(bool) # Default: true
     }))
   }))
 }
