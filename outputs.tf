9b6e311f01e268d2c96ce813e667ebed9c828a43
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:03:21 +0100

    Update to schema version v4.81.0

--- outputs.tf
+++ outputs.tf
@@ -52,7 +52,7 @@ output "iot_security_solutions_query_subscription_ids" {
 }
 output "iot_security_solutions_recommendations_enabled" {
   description = "Map of recommendations_enabled values across all iot_security_solutions, keyed the same as var.iot_security_solutions"
-  value       = { for k, v in azurerm_iot_security_solution.iot_security_solutions : k => v.recommendations_enabled if v.recommendations_enabled != null && length(v.recommendations_enabled) > 0 }
+  value       = { for k, v in azurerm_iot_security_solution.iot_security_solutions : k => one(v.recommendations_enabled) if v.recommendations_enabled != null && length(v.recommendations_enabled) > 0 }
 }
 output "iot_security_solutions_resource_group_name" {
   description = "Map of resource_group_name values across all iot_security_solutions, keyed the same as var.iot_security_solutions"
